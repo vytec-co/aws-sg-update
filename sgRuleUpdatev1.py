7697b644a15c334b016510e19fe6638675e6973a
vsm <devops.july2.2017@gmail.com>
2021-09-29 23:22:25 +0530

    Update sgRuleUpdatev1.py

--- sgRuleUpdatev1.py
+++ sgRuleUpdatev1.py
@@ -15,34 +15,51 @@ for i in response['SecurityGroups']:
     if i['GroupName'] == securityGroup:
        print("Security Group Name: "+i['GroupName'])
        print("The Ingress rules are as follows: ")
-       for j in i['IpPermissions']:
-          #print("IP Protocol: "+j['IpProtocol'])
-          try:
-           # print("PORT: "+str(j['FromPort']))
-            if str(j['FromPort']) == str(port_no):
-               print("PORT: "+str(j['FromPort']))
-               for k in j['IpRanges']:
-                 # print("IP Ranges: "+k['CidrIp'])
-                  ipSet.add(k['CidrIp']) 
-          except Exception:
-            print("No value for ports and ip ranges available for this security group")
-            continue
-          if ipAddress not in ipSet:
-            print("ip not exist and updating the rule with "+ipAddress)
+       if len(i['IpPermissions']) != 0:
+        for j in i['IpPermissions']:
+            #print("IP Protocol: "+j['IpProtocol'])
             try:
-                data = ec2.authorize_security_group_ingress(
-                    GroupId=i['GroupId'],
-                    IpPermissions=[
-                        {'IpProtocol': 'tcp',
-                        'FromPort': port_no,
-                        'ToPort': port_no,
-                        'IpRanges': [{'CidrIp': ipAddress}]}
-                    ])
-                ipSet.add(ipAddress)
+            # print("PORT: "+str(j['FromPort']))
+              if str(j['FromPort']) == str(port_no):
+                print("PORT: "+str(j['FromPort']))
+                for k in j['IpRanges']:
+                    # print("IP Ranges: "+k['CidrIp'])
+                    ipSet.add(k['CidrIp']) 
             except Exception:
-                print("Given IP is not proper format and should be 0.0.0.0/0 format")
+                print("No value for ports and ip ranges available for this security group")
                 continue
-          else:
-            print("ip already exist:"+ipAddress)
-          print("ip rules are:")
-          print(ipSet)
+            if ipAddress not in ipSet:
+                print("ip not exist and updating the rule with "+ipAddress)
+                try:
+                    data = ec2.authorize_security_group_ingress(
+                        GroupId=i['GroupId'],
+                        IpPermissions=[
+                            {'IpProtocol': 'tcp',
+                            'FromPort': port_no,
+                            'ToPort': port_no,
+                            'IpRanges': [{'CidrIp': ipAddress}]}
+                        ])
+                    ipSet.add(ipAddress)
+                except Exception:
+                    print("Given IP is not proper format and should be 0.0.0.0/0 format")
+                    continue
+            else:
+                print("ip already exist:"+ipAddress)
+            print("ip rules are:")
+            print(ipSet)
+       else:
+        try:
+            data = ec2.authorize_security_group_ingress(
+                GroupId=i['GroupId'],
+                IpPermissions=[
+                    {'IpProtocol': 'tcp',
+                    'FromPort': port_no,
+                    'ToPort': port_no,
+                    'IpRanges': [{'CidrIp': ipAddress}]}
+                ])
+            ipSet.add(ipAddress)
+            print("ip rules are:")
+            print(ipSet)
+        except Exception:
+            print("Given IP is not proper format and should be 0.0.0.0/0 format")
+            continue
