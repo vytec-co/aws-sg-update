c4e4887e28cade900730d54b1ac36864b5432016
Ubuntu <ubuntu@ip-172-31-18-28.us-east-2.compute.internal>
2021-10-08 11:22:23 +0000

    test

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 import boto3
+import json
 ipSet = set()
 json_data = []
 with open('addresses.json') as json_file:
@@ -9,32 +10,34 @@ with open('addresses.json') as json_file:
     print(item['description'])
     ipAddress = item['ip_prefix']
     description = item['description']
+    port_no = 443
     client = boto3.client('ec2')
     response = client.describe_security_groups(
         GroupNames=[
             'testgroup',
         ],
     )
-    print(response)
+    print("1")
     for i in response['SecurityGroups']:
+        print(i['GroupId'])
         if len(i['IpPermissions']) != 0:
             for j in i['IpPermissions']:
                 #print("IP Protocol: "+j['IpProtocol'])
                 try:
                 # print("PORT: "+str(j['FromPort']))
-                if str(j['FromPort']) == str(port_no):
-                    print("PORT: "+str(j['FromPort']))
-                    for k in j['IpRanges']:
-                        # print("IP Ranges: "+k['CidrIp'])
-                        ipSet.add(k['CidrIp']) 
-                except Exception:
-                    print("No value for ports and ip ranges available for this security group")
+                    if str(j['FromPort']) == str(port_no):
+                        print("PORT: "+str(j['FromPort']))
+                        for k in j['IpRanges']:
+                            # print("IP Ranges: "+k['CidrIp'])
+                            ipSet.add(k['CidrIp']) 
+                except Exception as e:
+                    print("No value for ports and ip ranges available for this security group"+e)
                     continue
                 if ipAddress not in ipSet:
                     print("ip not exist and updating the rule with "+ipAddress)
                     try:
-                        data = ec2.authorize_security_group_ingress(
-                            GroupId='sg-094fc5408aa7298c7',
+                        data = client.authorize_security_group_ingress(
+                            GroupId=i['GroupId'],
                             IpPermissions=[
                                 {'IpProtocol': 'tcp',
                                 'FromPort': port_no,
@@ -42,15 +45,15 @@ with open('addresses.json') as json_file:
                                 'IpRanges': [{'CidrIp': ipAddress, 'Description' : description}]}
                             ])
                         ipSet.add(ipAddress)
-                    except Exception:
-                        print("Given IP is not proper format and should be 0.0.0.0/0 format")
+                    except Exception as e:
+                        print("Given IP is not proper format and should be 0.0.0.0/0 format"+str(e))
                         continue
                 else:
-                    print("ip already exist:"+ipAddress)
-                
+                    print("ip already exist:"+ipAddress)      
         else:
             try:
-                data = ec2.authorize_security_group_ingress(
+                print("rule update started:"+ipAddress)
+                data = client.authorize_security_group_ingress(
                     GroupId=i['GroupId'],
                     IpPermissions=[
                         {'IpProtocol': 'tcp',
@@ -61,9 +64,10 @@ with open('addresses.json') as json_file:
                 ipSet.add(ipAddress)
                 print("ip rules are:")
                 print(ipSet)
-            except Exception:
-                print("Given IP is not proper format and should be 0.0.0.0/0 format")
+            except Exception as e:
+                print("Given IP is not proper format and should be 0.0.0.0/0 format"+str(e))
                 continue
         print("ip rules are:")
         print(ipSet)    
     print("updated") 
+
