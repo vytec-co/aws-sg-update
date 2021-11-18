eafefcf18bb8a401ded56a04bb935768c8185065
vsm <devops.july2.2017@gmail.com>
2021-11-18 22:34:07 +0530

    Update sg.ps1

--- describe-resources/sg.ps1
+++ describe-resources/sg.ps1
@@ -1,3 +1,3 @@
 foreach($line in Get-Content .\file.txt) {
-    aws ec2 describe-security-groups --group-ids $line | Out-File -FilePath .\output.txt
+    aws ec2 describe-security-groups --group-ids $line >> .\output.txt
 }
