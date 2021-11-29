8f9e704391200fc335c24d469114879c143fe3dc
vsm <devops.july2.2017@gmail.com>
2021-11-29 21:42:03 +0530

    Update all_ec2.sh

--- describe-resources/all_ec2.sh
+++ describe-resources/all_ec2.sh
@@ -1,1 +1,7 @@
-aws ec2 describe-instances --query 'Reservations[*].Instances[*].{InstanceId:InstanceId,Tags:Tags[?Key == `Name`] | [0].Value}' --output table
+//aws ec2 describe-instances --query 'Reservations[*].Instances[*].{InstanceId:InstanceId,Tags:Tags[?Key == `Name`] | [0].Value}' --output table
+
+for line in `cat input_file`
+do
+    echo "$line"
+    aws ec2 describe-instances --instance-id "$line" --query 'Reservations[*].Instances[*].{InstanceId:InstanceId,Tags:Tags[?Key == `Name`] | [0].Value}' --output table
+done
