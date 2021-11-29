f05a12a45484ccaea8ba9046c622939a630d0467
Ravi <Ravi@ip.com>
2021-11-29 22:18:28 +0530

    updated

--- describe-resources/shell/loggroup.sh
+++ describe-resources/shell/loggroup.sh
@@ -1,7 +1,5 @@
-aws logs describe-log-groups \
-	--log-group-name-prefix "Default"
 for line in `cat input_file`
 do
     echo "$line"
-    aws ec2 describe-instances --instance-id "$line" --query 'Reservations[*].Instances[*].{InstanceId:InstanceId,Tags:Tags[?Key == `Name`] | [0].Value}' --output table
+    aws logs describe-log-groups --log-group-name-prefix "Default"
 done
--- describe-resources/shell/volume.sh
+++ describe-resources/shell/volume.sh
@@ -1,11 +1,7 @@
-foreach($line in Get-Content .\file.txt) {
-    aws ec2 describe-volumes \
-    --volume-ids $line >> .\output.txt
-}
 for line in `cat input_file`
 do
     echo "$line"
-    aws ec2 describe-instances --instance-id "$line" --query 'Reservations[*].Instances[*].{InstanceId:InstanceId,Tags:Tags[?Key == `Name`] | [0].Value}' --output table
+    aws ec2 describe-volumes --volume-ids $line
 done
 
 
