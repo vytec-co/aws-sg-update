3cbaabca2afee376fe1e137d6e685577de0647a9
vsm <devops.july2.2017@gmail.com>
2021-11-29 18:24:50 +0530

    Update ec2.sh

--- describe-resources/ec2.sh
+++ describe-resources/ec2.sh
@@ -1,6 +1,1 @@
-
-
-while read -r line; do
-instanceid=$(aws ec2 describe-instances --instance-ids i-07d469ee66c1342d2 --query 'Reservations[0].Instances[0].InstanceId' --output text)
-echo $instanceid
-done <<< "$envinfo"
+aws ec2 describe-instances --query 'Reservations[*].Instances[*].{InstanceId:InstanceId,Tags:Tags[?Key == `Name`] | [0].Value}'
