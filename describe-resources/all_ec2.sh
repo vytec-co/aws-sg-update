244922a461d434e3a4b8b97a8d3701b15872fbfe
vsm <devops.july2.2017@gmail.com>
2021-11-29 18:30:16 +0530

    Update all_ec2.sh

--- describe-resources/all_ec2.sh
+++ describe-resources/all_ec2.sh
@@ -1,1 +1,1 @@
-aws ec2 describe-instances --query 'Reservations[*].Instances[*].{InstanceId:InstanceId,Tags:Tags[?Key == `Name`] | [0].Value}'
+aws ec2 describe-instances --query 'Reservations[*].Instances[*].{InstanceId:InstanceId,Tags:Tags[?Key == `Name`] | [0].Value}' --output table
