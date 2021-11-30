5a2aeef0f52e115d2abaa71fd659321a4f940e68
Ravi <Ravi@ip.com>
2021-11-30 17:19:13 +0530

    updayted

---
+++ describe-resources/shell/elb.sh
@@ -0,0 +1,6 @@
+
+for line in `cat elb_input_file`
+do
+    echo "$line"
+    aws elbv2 describe-load-balancers --names $line --query LoadBalancers[].{DNSName:DNSName,Tags:Tags[?Key == `Name`] | [0].Value}' --output table
+done
