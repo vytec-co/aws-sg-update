d78f96bc94595dfe5a4f2d5e1363cde2be2eb64d
Ravi <Ravi@ip.com>
2021-11-30 19:45:08 +0530

    updated

---
+++ describe-resources/shell/s3bucket.sh
@@ -0,0 +1,6 @@
+for line in `cat s3bucket_input_file`
+do
+    echo "$line"
+    aws s3api get-bucket-tagging --bucket $line --query 'TagSet[*].{Tags:Tags[?Key == `Name`] | [0].Value}' --output table
+done
+
